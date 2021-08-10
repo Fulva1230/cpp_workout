2470edc7275bfe26ebd5365bc03d48fd58066fc3
Fulva1230 <b10503137@gapps.ntust.edu.tw>
2021-08-10 21:37:31 +0800

    done the string mix

--- src/string_mix.cpp
+++ src/string_mix.cpp
@@ -18,15 +18,17 @@ using PreoutputEntryType = std::tuple<int, char, Owner>;
 
 class EntryCompare {
 public:
-  bool operator()(const EntryType &lhs, const EntryType &rhs) const {
-    if (lhs.second > rhs.second) {
-      return true;
-    } else if (lhs.second == rhs.second) {
-      if (lhs.first < rhs.first) {
-        return true;
+  bool operator()(const PreoutputEntryType &lhs,
+                  const PreoutputEntryType &rhs) const {
+    if (std::get<0>(lhs) == std::get<0>(rhs)) {
+      if (std::get<2>(lhs) == std::get<2>(rhs)) {
+        return std::get<1>(lhs) < std::get<1>(rhs);
+      } else {
+        return std::get<2>(lhs) < std::get<2>(rhs);
       }
+    } else {
+      return std::get<0>(lhs) > std::get<0>(rhs);
     }
-    return false;
   }
 };
 
@@ -46,6 +48,25 @@ void addIfGreater(CharCounterType &filled, const CharCounterType &refer) {
   }
 }
 
+void populateMixOrderedCounter(
+    CharCounterType &charCounter1, CharCounterType &charCounter2,
+    CharCounterType &mixCounter,
+    std::vector<PreoutputEntryType> &mixOrderedCounter) {
+  for (auto entry : mixCounter) {
+    int s1has = charCounter1[entry.first];
+    int s2has = charCounter2[entry.first];
+    if (entry.second > 1) {
+      if (s1has > s2has) {
+        mixOrderedCounter.emplace_back(s1has, entry.first, Owner::first);
+      } else if (s1has < s2has) {
+        mixOrderedCounter.emplace_back(s2has, entry.first, Owner::second);
+      } else {
+        mixOrderedCounter.emplace_back(entry.second, entry.first, Owner::both);
+      }
+    }
+  }
+}
+
 class Mix {
 public:
   static std::string mix(const std::string &s1, const std::string &s2) {
@@ -56,15 +77,13 @@ public:
     CharCounterType mixCounter{};
     addIfGreater(mixCounter, charCounter1);
     addIfGreater(mixCounter, charCounter2);
-    std::vector<std::pair<char, int>> mixOrderedCounter{std::cbegin(mixCounter),
-                                                        std::cend(mixCounter)};
+    std::vector<PreoutputEntryType> mixOrderedCounter{};
+    populateMixOrderedCounter(charCounter1, charCounter2, mixCounter,
+                              mixOrderedCounter);
+
     std::sort(std::begin(mixOrderedCounter), std::end(mixOrderedCounter),
               EntryCompare{});
     //    remove entry with count 1
-    auto newEnd = std::remove_if(
-        std::begin(mixOrderedCounter), std::end(mixOrderedCounter),
-        [](const EntryType &element) { return element.second <= 1; });
-    mixOrderedCounter.erase(newEnd, std::end(mixOrderedCounter));
     std::ostringstream output{};
     {
       bool firstRunFlag = true;
@@ -74,16 +93,18 @@ public:
         } else {
           output << '/';
         }
-        int s1has = charCounter1[entry.first];
-        int s2has = charCounter2[entry.first];
-        if (s1has > s2has) {
+        switch (std::get<2>(entry)) {
+        case Owner::first:
           output << "1:";
-        } else if (s1has < s2has) {
+          break;
+        case Owner::second:
           output << "2:";
-        } else {
+          break;
+        case Owner::both:
           output << "=:";
+          break;
         }
-        output << std::string(entry.second, entry.first);
+        output << std::string(std::get<0>(entry), std::get<1>(entry));
       }
     }
     return output.str();
@@ -94,10 +115,7 @@ TEST_CASE("test_string_mix") {
   REQUIRE(std::isblank(' ', std::locale()) == true);
 
   EntryCompare compare{};
-  REQUIRE(compare({'1', 3}, {'2', 5}) == false);
-  REQUIRE(compare({'a', 5}, {'d', 5}) == true);
-  REQUIRE(compare({'d', 6}, {'d', 5}) == true);
-  REQUIRE(compare({'b', 3}, {'b', 3}) == false);
+  REQUIRE(compare({3, '3', Owner::both}, {3, '5', Owner::both}) == true);
 
   REQUIRE(Mix::mix("Are they here", "yes, they are here") ==
           "2:eeeee/2:yy/=:hh/=:rr");
