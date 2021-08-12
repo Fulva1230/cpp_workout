1385f9cfbce18ded43b550b487191bdfcc5e2e81
Fulva1230 <b10503137@gapps.ntust.edu.tw>
2021-08-13 00:17:32 +0800

    improve the performance but still no get through the test

--- src/explorsive_sum.cpp
+++ src/explorsive_sum.cpp
@@ -2,43 +2,38 @@
 // Created by b1050 on 8/12/2021.
 //
 
+#include <iostream>
 #include <unordered_map>
 
 #include <catch2/catch_test_macros.hpp>
 
 using ull = unsigned long long;
 
-std::unordered_map<unsigned int, ull> cache{};
-
-ull exp_sum_without_lowerequal(unsigned int n, unsigned int wo) {
-  if (wo == 1) {
-    return cache[n] - cache[n - 1];
-  } else {
-    ull sum = cache[n] - cache[n - 1];
-    for (int wx = 2; wx <= wo; ++wx) {
-      sum -= exp_sum_without_lowerequal(n - wx, wx - 1);
-    }
-    return sum;
-  }
-}
+std::unordered_map<unsigned int, std::unordered_map<unsigned int, ull>> cache{};
 
 ull exp_sum(unsigned int n) {
-  cache[0] = 1;
-  cache[1] = 1;
-  for (unsigned int i = 2; i <= n; ++i) {
-    cache[i] = 1;
-    cache[i] += cache[i - 1];
-    for (int j = 2; j <= (i / 2); ++j) {
-      cache[i] += exp_sum_without_lowerequal(i - j, j - 1);
+  cache[0][0] = 1;
+  cache[1][0] = 1;
+  for (int i = 2; i <= n; ++i) {
+    for (int j = 0; j < i; ++j) {
+      cache[i][j] = 1;
+      for (int q = 1; q <= (i / 2) - j; ++q) {
+        cache[i][j] += cache[i - j - q][j + q - 1];
+      }
     }
   }
-  return cache[n];
+  return cache[n][0];
 }
 
 TEST_CASE("exp_sum") {
+  CHECK(exp_sum(2) == 2);
+  CHECK(exp_sum(3) == 3);
   CHECK(exp_sum(5) == 7);
   CHECK(exp_sum(6) == 11);
   CHECK(exp_sum(8) == 22);
+
   CHECK(exp_sum(10) == 42);
   CHECK(exp_sum(20) == 627);
+  CHECK(exp_sum(100) == 190569292);
+  CHECK(exp_sum(230) == 47826239745920);
 }
